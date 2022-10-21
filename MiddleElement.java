bfd2c6920bc1c58c7fbb30fec29fda2de9bbcf23
raguramMurugan <116337952+raguramMurugan@users.noreply.github.com>
2022-10-21 15:13:55 +0530

    Full Coding Updated
    I have created a cllass for LinkedList, 
    Please kindly create a separate class for Node and try to Try and execute the output and check.

--- MiddleElement.java
+++ MiddleElement.java
@@ -1,8 +1,56 @@
 package programforpractice;
 
-public class MiddleElement {
+public class LinkedList {
 
+	public Node head;
+	LinkedList()
+	{
+		this.head=null;
+	}
+	public Node insertData()
+	{
+		if(head!=null)
+		{
+			head=new Node(data);
+		}else
+		{
+			Node temp=new Node(data);
+			temp.next=head;
+			head=temp;
+		}
+		return head;
+	}
+	public void print()
+	{
+		Node temp=head;
+		while(temp!null)
+		{
+			System.out.println(temp.data);
+			temp=temp.next;
+		}
+	}
+	
+	public void middleElementFound()
+	{
+		Node fast=head;
+		Node slow=head;
+		
+		while(fast!=null && fast.next!=null)
+		{
+			slow=slow.next;
+			fast=fast.next.next;
+		}
+		System.out.println(slow.data);
+	}
+	
 	public static void main(String[] args) {
+		LinkedList ll= new LinkedList();
+		ll.insertData(6);
+		ll.insertData(7);
+		ll.insertData(9);
+		ll.insertData(5);
+		ll.insertData(8);
+		ll.middleElementFound();
 		
 	}
 }
